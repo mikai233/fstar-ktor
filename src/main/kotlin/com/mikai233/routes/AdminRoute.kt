8ca61f87437be29affe41ef536ba19a458cca5c4
dreamfever <dreamfever2017@yahoo.com>
2021-09-01 21:26:33 +0800

    9/1/2021

--- src/main/kotlin/com/mikai233/routes/AdminRoute.kt
+++ src/main/kotlin/com/mikai233/routes/AdminRoute.kt
@@ -3,7 +3,9 @@
 package com.mikai233.routes
 
 import com.mikai233.orm.CommonResult
+import com.mikai233.orm.Device
 import com.mikai233.orm.LoginRequest
+import com.mikai233.service.deviceService
 import com.mikai233.service.redisService
 import com.mikai233.service.userService
 import com.mikai233.tool.generateToken
@@ -15,6 +17,8 @@ import io.ktor.auth.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import java.time.Instant
+import java.time.LocalDate
 
 /**
  * @author mikai233
@@ -55,6 +59,7 @@ fun Application.adminRoute() {
              * 需要鉴权的接口
              */
             authenticate("ROLE_ADMIN") {
+                //用户统计接口
                 route("/user") {
                     /**
                      * 分页获取用户信息
@@ -96,6 +101,7 @@ fun Application.adminRoute() {
                         call.respond(CommonResult(data = user))
                     }
                 }
+                //活跃度统计接口
                 route("/vitality") {
                     get("/day") {
                         redisService.currentDayVitality().size.also {
@@ -127,6 +133,85 @@ fun Application.adminRoute() {
                             redisService.currentMonthVitality().also {
                                 call.respond(CommonResult(data = it))
                             }
+                        }
+                    }
+                    /**
+                     * 传入具体的Unix时间戳，返回天、周、月范围内的活跃度
+                     */
+                    route("/concretely") {
+                        //具体到某一天的活跃度，毫秒级Unix时间戳
+                        get("/day") {
+                            val timestamp = call.request.queryParameters["timestamp"]?.toLongOrNull()
+                            if (timestamp == null) {
+                                call.respond(requestParamInvalid)
+                                return@get
+                            }
+                            redisService.dayVitality(LocalDate.from(Instant.ofEpochMilli(timestamp)))
+                        }
+                        //具体到某一周的活跃度，毫秒级Unix时间戳
+                        get("/week") {
+                            val timestamp = call.request.queryParameters["timestamp"]?.toLongOrNull()
+                            if (timestamp == null) {
+                                call.respond(requestParamInvalid)
+                                return@get
+                            }
+                            redisService.weekVitality(LocalDate.from(Instant.ofEpochMilli(timestamp)))
+                        }
+                        //具体到某一月的活跃度，毫秒级Unix时间戳
+                        get("/month") {
+                            val timestamp = call.request.queryParameters["timestamp"]?.toLongOrNull()
+                            if (timestamp == null) {
+                                call.respond(requestParamInvalid)
+                                return@get
+                            }
+                            redisService.monthVitality(LocalDate.from(Instant.ofEpochMilli(timestamp)))
+                        }
+                    }
+                }
+                //设备统计接口
+                route("/device") {
+                    get {
+                        val queryParameters = call.request.queryParameters
+                        val page = queryParameters["page"]?.toIntOrNull()
+                        val size = queryParameters["size"]?.toIntOrNull()
+                        if (page == null || size == null || page < 0 || size < 0) {
+                            call.respond(requestParamInvalid)
+                            return@get
+                        }
+                        deviceService.getDevicesByPage(page, size).also {
+                            call.respond(CommonResult(data = it))
+                        }
+                    }
+                    get("/id/{id}") {
+                        val id = call.parameters["id"]?.toIntOrNull()
+                        if (id == null) {
+                            call.respond(requestParamInvalid)
+                            return@get
+                        }
+                        deviceService.getDeviceById(id).also {
+                            call.respond(CommonResult(data = it))
+                        }
+                    }
+                    get("/android_id/{androidId}") {
+                        val androidId = call.parameters["androidId"]
+                        if (androidId == null) {
+                            call.respond(requestParamInvalid)
+                            return@get
+                        }
+                        deviceService.getDeviceByAndroidId(androidId).also {
+                            call.respond(CommonResult(data = it))
+                        }
+                    }
+                    post {
+                        val device = call.receive<Device>()
+                        deviceService.addDevice(device).also {
+                            call.respond(CommonResult(data = it))
+                        }
+                    }
+                    patch {
+                        val device = call.receive<Device>()
+                        deviceService.updateDevice(device).also {
+                            call.respond(CommonResult(data = it))
                         }
                     }
                 }
